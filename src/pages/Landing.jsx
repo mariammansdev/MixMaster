f2b2f7059f21215355597b68958789f31e39dacb
Rana Mohamed <rana.mohamed@axisdata.net>
2025-07-28 16:36:48 +0200

    Fetch Drinks

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,19 @@
 
+import axios from "axios";
 import { useLoaderData } from "react-router-dom";
 
-export const loader = ()=>{
-  return (<h1>Loader</h1>)
+const cockTailUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+
+export const Loader = async () => {
+  const searchQuery = 'margarita';
+  const res = await axios.get(`${cockTailUrl}${searchQuery}`);
+  const data = res.data.drinks;
+
+  return { drinks: data ? data : [], searchQuery }
 }
 const Landing = () => {
+  const { drinks, searchQuery } = useLoaderData();
+  console.log(drinks, searchQuery);
   return (
     <>
       <h1>Landing </h1>
